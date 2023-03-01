da371e4ed5475b2f2d2c67d413e50ab9714c8a14
Brandon Corfman <bcorfman@fastmail.fm>
2023-03-01 19:37:56 +0000

    revised Australia test problem

--- tests/test_csplib.py
+++ tests/test_csplib.py
@@ -4,11 +4,13 @@ from core.csplib import Problem
 def test_problem():
     # Australia map-coloring problem
     vars = ['wa', 'nt', 'sa', 'ql', 'nsw', 'v', 't']
+    neighbors = {'sa': ['wa', 'nt', 'ql', 'nsw', 'v'], 'wa': ['nt', 'sa'],
+                 'nt': ['ql', 'sa', 'wa'], 'ql': ['nt', 'nsw', 'sa'],
+                 'nsw': ['v', 'ql', 'sa'], 'v': ['sa', 'nsw'], 't': []}
     domains = [['green', 'red', 'blue'] for _ in range(len(vars))]
-    constraints = '''sa != wa and sa != nt and sa != ql and sa != nsw
-                     sa != ql and sa != v and wa != nt and nt != ql and
-                     ql != nsw and nsw != v'''
-    problem = Problem(vars, domains, constraints)
+    constraints = ['sa != wa', 'sa != nt', 'sa != ql', 'sa != nsw', 'sa != v',
+                   'wa != nt', 'nt != ql', 'ql != nsw', 'nsw != v']
+    problem = Problem(vars, domains, neighbors, constraints)
     assert problem.solve() == {'wa': 'red', 'nt': 'green', 'sa': 'blue',
                                'ql': 'red', 'nsw': 'green', 'v': 'red',
                                't': 'green'}
